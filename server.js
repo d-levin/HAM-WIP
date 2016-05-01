ebea62e58918d1ffe1178554c7928d64e3169de1
d-levin <dennis.a.levin@gmail.com>
2016-05-01 02:13:06 -0700

    Logic for getting all users/controllers

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var port = process.env.PORT || 8080;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+// app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 /*=====  End of Dependencies  ======*/
 
 /* Keep track of base directory to avoid long include-paths */
