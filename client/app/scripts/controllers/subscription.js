99ccc43912bc9092683114162bc03974db73259f
d-levin <dennis.a.levin@gmail.com>
2016-06-01 15:59:38 -0700

    premium memberships

--- client/app/scripts/controllers/subscription.js
+++ client/app/scripts/controllers/subscription.js
@@ -8,11 +8,51 @@
  * Controller of the app
  */
 angular.module('app')
-  .controller('SubscriptionCtrl', ['$scope', '$rootScope',
-    function($scope, $rootScope) {
+  .controller('SubscriptionCtrl', ['$scope', '$rootScope', '$http', '$cookies', '$timeout', '$location',
+    function($scope, $rootScope, $http, $cookies, $timeout, $location) {
 
       $scope.onInit = function() {
         $rootScope.currentView = 'Manage Subscription';
+      };
+
+      $scope.purchase = function() {
+        var params = 'premium=true';
+        var config = {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        };
+        $http.put('/users/' + $cookies.get('userId'), params, config)
+          .success(function() {
+            $scope.updated = true;
+            $timeout(function() {
+              $rootScope.premium = true;
+              $location.path('/dashboard');
+            }, 2000);
+          })
+          .error(function() {
+            $scope.updated = false;
+          });
+      };
+
+      $scope.cancel = function() {
+        var params = 'premium=false';
+        var config = {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+        };
+        $http.put('/users/' + $cookies.get('userId'), params, config)
+          .success(function() {
+            $scope.updated = true;
+            $timeout(function() {
+              $rootScope.premium = false;
+              $location.path('/dashboard');
+            }, 2000);
+          })
+          .error(function() {
+            $scope.updated = false;
+          });
       };
     }
   ]);
